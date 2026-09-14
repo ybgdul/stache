3b8a7c14c62774678e6a6995124faaa37772bcc6
Abdurakhim <abdurakhim.zhappar@nu.edu.kz>
2026-09-14 09:10:44 +0500

    Server and protocol full implementation

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -1,6 +1,7 @@
 package protocol
 
 import (
+	"encoding/binary"
 	"io"
 	"time"
 )
@@ -31,15 +32,128 @@ type Response struct {
 	Err string 
 }
 
-func WriteRequest(w io.Writer, req *Request) { 
+func WriteRequest(w io.Writer, req *Request) error { 
+	if err := binary.Write(w, binary.BigEndian, req.Cmd); err != nil { 
+		return err
+	}
 
+	keyBytes := []byte(req.Key)
+	if err := binary.Write(w, binary.BigEndian, uint32(len(keyBytes))); err != nil { 
+		return err
+	}
+	if len(keyBytes) > 0 { 
+		if _, err := w.Write(keyBytes); err != nil { 
+			return nil
+		}
+	}
+
+	if err := binary.Write(w, binary.BigEndian, req.TTL.Nanoseconds()); err != nil { 
+		return err
+	}
+
+	if err := binary.Write(w, binary.BigEndian, uint32(len(req.Value))); err != nil { 
+		return err
+	}
+
+	if len(req.Value) > 0 { 
+		if _, err := w.Write(req.Value); err != nil { 
+			return err
+		}
+	}
+
+	return nil
 }
-func ReadRequest(r io.Reader) { 
+func ReadRequest(r io.Reader) (*Request, error){ 
+	req := &Request{}
 
+	if err := binary.Read(r, binary.BigEndian, &req.Cmd); err != nil { 
+		return nil, err
+	} 
+
+	var keyLen uint32 
+	if err := binary.Read(r, binary.BigEndian, &keyLen); err != nil { 
+		return nil, err
+	}
+	if keyLen > 0 { 
+		keyBuf := make([]byte, keyLen)
+		if _, err := io.ReadFull(r, keyBuf); err != nil { 
+			return nil, err
+		}
+		req.Key = string(keyBuf)
+	}
+	var ttlNano time.Duration
+	if err := binary.Read(r, binary.BigEndian, &ttlNano); err != nil { 
+		return nil, err
+	}
+	req.TTL = ttlNano
+
+	var valLen uint32
+	if err := binary.Read(r, binary.BigEndian, &valLen); err != nil { 
+		return nil, err
+	}
+	if valLen > 0 { 
+		req.Value = make([]byte, valLen)
+		if _, err := io.ReadFull(r, req.Value); err != nil { 
+			return nil, err
+		}
+	}
+
+	return req, nil
 }
-func WriteResponse(w io.Writer, resp *Response) { 
+func WriteResponse(w io.Writer, resp *Response) error { 
+	if err := binary.Write(w, binary.BigEndian, resp.Status); err != nil {
+		return err
+	}
 
+	errBytes := []byte(resp.Err)
+	if err := binary.Write(w, binary.BigEndian, uint32(len(errBytes))); err != nil { 
+		return err
+	}
+	if len(errBytes) > 0 { 
+		if _, err := w.Write(errBytes); err != nil { 
+			return err
+		}
+	}
+	if err := binary.Write(w, binary.BigEndian, uint32(len(resp.Value))); err != nil { 
+		return err 
+	}
+	if len(resp.Value) > 0 { 
+		if _, err := w.Write(resp.Value); err != nil { 
+			return err 
+		}
+	}
+
+	return nil
 }
-func ReadResponse(r io.Reader) { 
+func ReadResponse(r io.Reader) (*Response, error){ 
+	resp := &Response{}
+	if err := binary.Read(r, binary.BigEndian, &resp.Status ); err != nil { 
+		return nil, err
+	}
 
+	var errLen uint32
+	if err := binary.Read(r, binary.BigEndian, &errLen); err != nil { 
+		return nil, err
+	}
+	if errLen > 0 { 
+		errBuf := make([]byte, errLen)
+		if _, err := io.ReadFull(r, errBuf); err != nil { 
+			return nil, err 
+		}
+		resp.Err = string(errBuf)
+	}
+
+	var valLen uint32
+	if err := binary.Read(r, binary.BigEndian, &valLen); err != nil { 
+		return nil, err 
+	}
+	if valLen > 0 { 
+		valBuf := make([]byte, valLen)
+		if _, err := io.ReadFull(r, valBuf); err != nil { 
+			return nil, err 
+		}
+		resp.Value = valBuf
+	}
+
+	return resp, nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"errors"
+	"io"
 	"net"
+	"os"
 	"stache/internal/engine"
+	"stache/internal/protocol"
 	"sync"
 )
 
@@ -20,4 +24,98 @@ func New(socketPath string, cache *engine.Cache) *Server {
 		cache: cache,
 		quit: make(chan struct{}),
 	}
+}
+
+func (s *Server) Start() error { 
+	if err := os.Remove(s.socketPath); err != nil && os.IsNotExist(err){ 
+		return err 
+	}
+
+	l, err := net.Listen("unix", s.socketPath)
+	if err != nil { 
+		return err 
+	}
+	s.listener = l
+
+	s.wg.Add(1)
+	go s.acceptLoop()
+
+	return nil
+}
+
+func (s *Server) acceptLoop() { 
+	defer s.wg.Done()
+
+	for { 
+		conn, err := s.listener.Accept()
+		if err != nil { 
+			select { 
+			case <-s.quit: 
+				return
+			default: 
+				continue 
+			}
+		}
+		s.wg.Add(1)
+
+		go func(c net.Conn) {
+			defer s.wg.Done()
+			s.handleConn(c)
+		}(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) { 
+	defer conn.Close() 
+	
+	for { 
+		req, err := protocol.ReadRequest(conn)
+		if err != nil { 
+			if errors.Is(err, io.EOF) { 
+				return 
+			}
+			return
+		}
+
+		resp := s.dispatch(req) 
+		if err := protocol.WriteResponse(conn, resp); err != nil { 
+			return 
+		}
+	}
+}
+
+func (s *Server) dispatch(req *protocol.Request) *protocol.Response { 
+	switch req.Cmd {
+	case protocol.CmdGet: 
+		val, found := s.cache.Get(req.Key)
+		if !found {
+			return &protocol.Response{Status: protocol.StatusNotFound}
+		}
+		return &protocol.Response{Status: protocol.StatusOk, Value: val}
+	case protocol.CmdSet: 
+		err := s.cache.Set(req.Key, req.Value, req.TTL); 
+		if err != nil { 
+			return &protocol.Response{Status: protocol.StatusErr, Err: err.Error("can't set cache")}
+		}
+		return &protocol.Response{Status: protocol.StatusOk}
+	case protocol.CmdDelete: 
+		s.cache.Delete(req.Key)
+		return &protocol.Response{Status: protocol.StatusOk}
+	case protocol.CmdClear: 
+		s.cache.Clear()
+		return &protocol.Response{Status: protocol.StatusOk}
+	default:
+		return &protocol.Response{Status: protocol.StatusErr, Err: "unknown command"}
+	}
+}
+
+func (s *Server) Stop() error { 
+	close(s.quit)
+	var err error
+	if s.listener != nil { 
+		err = s.listener.Close()
+		os.Remove(s.socketPath)
+	}
+	s.wg.Wait()
+	return err 
 }
