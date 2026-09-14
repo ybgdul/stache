f50da9c9072769b42d1f514a414dbb1aa9f7cb89
Abdurakhim <abdurakhim.zhappar@nu.edu.kz>
2026-09-14 17:30:09 +0500

    Daemon server iniitalization and cli tool's basic commands

--- cmd /stache-daemon/main.go
+++ cmd /stache-daemon/main.go
@@ -1,1 +1,52 @@
-package stachedaemon
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"stache/internal/engine"
+	"stache/internal/server"
+	"syscall"
+	"time"
+)
+
+func main() { 
+	socketPath := flag.String("socket", "/tmp/stache.sock", "Path to Unix Domain Socket")
+	maxMem := flag.Int64("max-memory", 256*1024*1024, "Max memory in bytes (Default - 256MB)")
+	maxItem := flag.Int64("max-item-size", 4*1024*1024, "Max single item size in bytes (Default - 4MB)")
+
+	flag.Parse()
+
+	log.Printf("Starting stache daemon (socket: %s, max-memory: %dMB)", *socketPath, *maxMem)
+
+	cacheEngine := engine.New(
+		engine.LimitsConfig{
+			MaxMemoryBytes: *maxMem,
+			MaxItemBytes: *maxItem,
+			CleanUpInterval: 1 * time.Minute,
+		},
+	)
+	defer cacheEngine.Close()
+
+	s := server.New(
+		*socketPath,
+		cacheEngine,
+	)
+	if err := s.Start(); err != nil { 
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+	log.Printf("stache daemon is ready for connections")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutting the stache daemon now...")
+
+	if err := s.Stop(); err != nil { 
+		log.Printf("Can't shut the daemon: %v", err)
+	}
+
+	log.Println("Daemon stopped succesfully")
+}
--- cmd /stache/main.go
+++ cmd /stache/main.go
@@ -1,1 +1,65 @@
-package stache
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"stache/internal/protocol"
+	"time"
+)
+
+func main() { 
+	if len(os.Args) < 2 { 
+		printUsage()
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+	socketPath := "/tmp/stache.sock"
+
+	conn, err := net.DialTimeout("unix", socketPath, 2*time.Second)
+
+	if err != nil {
+		fmt.Printf("Error: couldn't connect to daemon at %s: %v\n", socketPath, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	switch cmd {
+	case "clear": 
+		req := &protocol.Request{
+			Cmd: protocol.CmdClear,
+		}
+		if err := protocol.WriteRequest(conn, req); err != nil { 
+			fmt.Printf("Error sending command: %v\n", err)
+			os.Exit(1)
+		}
+
+		resp, err := protocol.ReadResponse(conn)
+		if err != nil { 
+			fmt.Printf("Error reading response: %v\n", err)
+			os.Exit(1)
+		}
+
+		if resp.Status == protocol.StatusOk {
+			fmt.Println("Cache cleared succesfully")
+		} else { 
+			fmt.Printf("Failed to clear cache: %v\n", resp.Err)
+		}
+
+	case "status": 
+		fmt.Println("stache daemon is running and reachable")
+	default: 
+		fmt.Println("Unknown command: %s\n", cmd)
+		printUsage()
+		os.Exit(1)
+	}
+
+}
+
+func printUsage() { 
+	fmt.Println("Helper:")
+	fmt.Println("Usage: stache <command>")
+	fmt.Println("Commands:")
+	fmt.Println("  status  Check connectivity")
+	fmt.Println("  clear   Clear all cached items")
+}
